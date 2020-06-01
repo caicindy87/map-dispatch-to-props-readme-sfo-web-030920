e71b901f0251f367efe8a728998de76e40e92eae
caicindy87 <caicindy87@gmail.com>
2020-05-31 18:06:46 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import React, { Component } from 'react';
-import './App.css';
-import { connect } from 'react-redux';
-import { addItem } from  './actions/items';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import "./App.css";
+// importing action creator from separate file
+import { addItem } from "./actions/items";
 
 class App extends Component {
-
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    // don't have to write this.props.dispatch(addItem()) = default dispatch behavior
+    this.props.addItem();
   }
 
   render() {
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
-          Click
-          </button>
+        <button onClick={(event) => this.handleOnClick(event)}>Click</button>
         <p>{this.props.items.length}</p>
       </div>
     );
   }
-};
+}
 
 const mapStateToProps = (state) => {
   return {
-    items: state.items
+    items: state.items,
   };
 };
 
-export default connect(mapStateToProps)(App);
+// takes in dispatch as an argument
+// mapDispatchToProps returns an object with a function as a value
+// will map the addItem key/value pair to App component's props
+// then in handleOnClick, the function we saved as a value is called, not the addItem() action creator
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addItem: () => dispatch(addItem()),
+  };
+};
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// this is the same thing as if we passed in mapDispatchToProps
+export default connect(mapStateToProps, { addItem })(App);
